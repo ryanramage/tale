5dca23ae08f51e55169ef70433223c8e4b737f47
Ryan Ramage <ryan.ramage@gmail.com>
2014-03-10 21:10:20 -0600

    small changes to have all packaged up into ine file

--- html/js/app.js
+++ html/js/app.js
@@ -1,4 +1,23 @@
 define(['oboe'], function(oboe){
 
+  console.log('ok, here we go');
 
+
+  window.applicationCache.addEventListener('cached', function(){
+    console.log('all done');
+  }, false);
+  window.applicationCache.addEventListener('noupdate', function(){
+    console.log('loaded prev');
+  }, false)
+
+  window.applicationCache.addEventListener('downloading', function(){
+    console.log('starting to download');
+  }, false);
+
+  window.applicationCache.addEventListener('progress', function(e){
+    console.log('downloading', (e.loaded / e.total) * 100, '%' ) ;
+  }, false);
+
+
+  return {};
 })
